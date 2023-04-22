7bcf138c0c86385d572f2ffe7021eca7eb25e623
Franklin <franklinpenalvercollazo@gmail.com>
2023-04-22 01:13:12 -0400

    Update Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,22 @@
 import { ActiveLink } from "./ActiveLink";
 import React from "react";
 import styles from "./Navbar.module.css";
+import Link from "next/link"
 
 export const Navbar = () => {
   return (
-    <nav className={styles.menu_container}>
-      <ActiveLink text="Estudios de caso" href="/context/ContextList" />
-      <ActiveLink text="About" href="/about" />
-      <ActiveLink text="Contacto" href="/contact" />
+    <nav >
+      <ul className="flex border-b">
+  <li className="-mb-px mr-1">
+    <Link className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"  href="/context/ContextList">Estudios de caso</Link>
+  </li>
+  <li class="mr-1">
+    <Link class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="/about">Acerca de</Link>
+  </li>
+  <li class="mr-1">
+    <Link class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="/contact">Contacto</Link>
+  </li>
+</ul>
     </nav>
   );
 };
